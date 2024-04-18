6450b616308ee6d77b0df423c6c0773ffae107b2
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-18 13:42:25 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by csuftitan on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
