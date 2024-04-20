53a21ef64ac25b8671cde8d97163516c3a26d764
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-19 23:49:08 -0700

    create view to add books

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,26 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by csuftitan on 4/19/24.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = "Fantasy"
+    @State private var review = ""
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
