605e1454ab52ae5d02e159444da1221b7ae925dc
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-20 02:40:56 -0700

    update add book view

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -16,8 +16,42 @@ struct AddBookView: View {
     @State private var genre = "Fantasy"
     @State private var review = ""
     
+    let genres = ["Fantasy", "Horror", "Kids", "Mystery", "Poetry", "Romance", "Thriller"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+
+                Section("Write a review") {
+                    TextEditor(text: $review)
+
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                }
+
+                Section {
+                    Button("Save") {
+                        // add the book
+                        let newBook = Book(title: title, author: author, genre: genre, review: review, rating: rating)
+                        modelContext.insert(newBook)
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
     }
 }
 
