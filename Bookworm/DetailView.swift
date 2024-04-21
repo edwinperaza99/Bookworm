1b976640e9c5ea24af6eada6007a19c6d23baabb
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-21 15:03:32 -0700

    add images

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Edwin on 4/21/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
